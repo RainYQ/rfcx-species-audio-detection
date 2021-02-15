fce45065448fe207d8056ff308a67e20e542f425
RainYQ <39025827+RainYQ@users.noreply.github.com>
2021-02-15 13:26:36 +0800

    change BN→GN & Add label smooth & using model_plot to show model information

--- ensemble.py
+++ ensemble.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
 
-def ensemble(DataFrame1, DataFrame2, DataFrame3):
+def ensemble(DataFrame):
     for i in range(24):
         name = "s" + str(i)
-        DataFrame1[name] = 1 / 3 * (DataFrame1[name] + 1 / 3 * DataFrame2[name] + 1 / 3 * DataFrame3[name])
-    return DataFrame1
+        for j in range(len(DataFrame) - 1):
+            DataFrame[0][name] += DataFrame[j + 1][name]
+        DataFrame[0][name] = 1 / len(DataFrame) * DataFrame[0][name]
+    return DataFrame[0]
 
 
 workdir = "D://RainYQ/rfcx-species-audio-detection/result/"
-DataFrame1 = pd.read_csv(workdir + "submission_0.csv")
-DataFrame2 = pd.read_csv(workdir + "submission_1.csv")
-DataFrame3 = pd.read_csv(workdir + "submission_2.csv")
-ensemble(DataFrame1, DataFrame2, DataFrame3).to_csv(workdir + "submission_ensemble.csv", index=False)
+DataFrame = []
+for i in range(9):
+    if i == 0 or i == 1:
+        pass
+    else:
+        DataFrame.append(pd.read_csv(workdir + "submission_" + str(i) + ".csv"))
+ensemble(DataFrame).to_csv(workdir + "submission_ensemble.csv", index=False)
